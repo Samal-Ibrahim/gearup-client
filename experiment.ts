8272c02ee516b724831dbadadaba7e6f6094328e
Samal <56509582+FransoArbela@users.noreply.github.com>
2026-07-12 19:20:32 +0200

    feat: remove unused experiment.ts file and add experiment.js; update tsconfig for strict type checking

--- experiment.ts
+++
@@ -1,6 +0,0 @@
-type State = |  {status: "loading";}   |   { status: "success"; data: { id: string;}; }    |   { status: "error"; error: Error;} 
-
-const state: State = {
-    status: "success",
-    data: {id: "3"}
-}
