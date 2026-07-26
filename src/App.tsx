9af919eba54370e4f56fce3e842a72a3ce622475
Samal <56509582+FransoArbela@users.noreply.github.com>
2026-07-26 18:16:04 +0200

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import AppRoutes from "./routes/Index"
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
+      <div className="flex w-full items-center bg-green-50 p-6 shadow-md">
+        <h3 className="mx-auto">Work in progress... 🙏👇</h3>
+      </div>
       <Header />
       <main className="flex-1">
         <AppRoutes />
